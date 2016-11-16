1144027a7678c95186b0fd16814abf61c8a52efb
Oamazing <2569202365@qq.com>
2016-11-16 18:25:22 +0800

    one more  loading  animation

---
+++ loading/js/canvas.js
@@ -0,0 +1,6 @@
+var canvas = document.querySelector('#loading6');
+var ct = canvas.getContext('2d');
+ct.strokeStyle = "#5d9cec";
+ct.lineWidth = 10;
+ct.arc(50,50,40,0,Math.PI*1.8,false);
+ct.stroke();
