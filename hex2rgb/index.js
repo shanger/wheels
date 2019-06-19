0633118fa5fddb7f123e1392be52e2d955428871
单国力 <15800389618@163.com>
2019-06-19 15:40:37 +0800

    代码简化

--- hex2rgb/index.js
+++ hex2rgb/index.js
@@ -12,9 +12,9 @@ function hex2rgb(hex){
     var colorStr = hex.slice(1)
     var fColorStr = colorStr
     if(colorStr.length === 3){
-        fColorStr = colorStr.replace(/([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/,'$1$1$2$2$3$3')
+        fColorStr = colorStr.replace(/([a-fA-F0-9])/g,'$1$1')
     }
-    return 'rgb('+fColorStr.split(/(?:([a-fA-F0-9]{2}))/).filter(ele=>{return ele}).map(ele=>{
+    return 'rgb('+fColorStr.match(/[a-fA-F0-9]{2}/g).map(ele=>{
         return parseInt(ele,16)
         }).join(',')+')'
 }
